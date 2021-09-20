f531ade1eadfbf00444e6a82caf6d936c5dc27c8
Krisquin aka Jerquin <72782909+jerkin61@users.noreply.github.com>
2021-09-20 09:56:20 +0800

    Uploaded

---
+++ test.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<head> Wala lang</head>
+
+<form action="">
+    <div class="input-box">
+        <input type="text" name="username" autocomplete="off" required>
+        <label for =""> Username</label>
+    
+    </div>
+
+
+</form>
+</html>
+
+<?php 
